27471655f2b043bbad34b06e8e884e15f2c8846f
leftie457 <leftie457@gmail.com>
2019-04-12 16:13:48 -0600

    Update resume.js

--- resume.js
+++ resume.js
@@ -8,7 +8,7 @@ function createPersonalInfoSection(personalInfo) {
     <h2>${resume.personalInfo.name}</h2>
     <p>${resume.personalInfo.phone}</p>
     <p>${resume.personalInfo.email}</p>
-    <link href="${resume.personalInfo.gitHub}">GitHub: </link>`;
+    <a href="${resume.personalInfo.gitHub}">GitHub: </a>`;
     return personalInfoString;
 }
 
